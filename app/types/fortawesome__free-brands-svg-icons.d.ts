a8afe406d50ddeb3d52a9914ea0cd2a8527df5ed
Avisi Sachan <avisachan07@.com>
2024-05-07 19:04:14 +0530

    Add declaration file for @fortawesome/free-brands-svg-icons

---
+++ app/types/fortawesome__free-brands-svg-icons.d.ts
@@ -0,0 +1,9 @@
+// fortawesome__free-brands-svg-icons.d.ts
+
+declare module '@fortawesome/free-brands-svg-icons' {
+    import { IconDefinition } from '@fortawesome/fontawesome-common-types';
+
+    export const faFontAwesome: IconDefinition;
+    export const faTwitter: IconDefinition;
+    export const faFacebook: IconDefinition;
+    // Add more icon declarations as needed
